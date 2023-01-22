1631c31573ecbec4523faf015d93b3b577dfba96
alpakas96 <jhurleyfilm@gmail.com>
2023-01-22 15:00:47 -0500

    working on UI

---
+++ src/HamburgerMenu.jsx
@@ -0,0 +1,11 @@
+import './HamburgerMenu.css' 
+
+export default function HamburgerMenu(fruitArray) {
+    return (
+        <aside className="hamburgerMenu">
+            <div className="menuItem"> Banana </div>
+            <div className="menuItem"> Apple </div>
+            <div className="menuItem"> Grape </div>
+        </aside>
+    )
+}
